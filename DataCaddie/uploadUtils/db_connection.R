589e5968a6348094f434fe51fdb3d0dd2f10f4aa
SethJ50 <139821336+SethJ50@users.noreply.github.com>
2025-10-30 15:00:58 -0700

    changed connection to use atlas

--- DataCaddie/uploadUtils/db_connection.R
+++ DataCaddie/uploadUtils/db_connection.R
@@ -5,16 +5,7 @@ get_mongo_collection <- function(collection_name, db_name = "data_caddy") {
     readRenviron(".Renviron")
   }
   
-  username <- "admin"
-  password <- Sys.getenv("ADMIN_PWD")
-  host <- "localhost"
-  port <- 27017
-  auth_db <- "admin"
-  
-  db_url <- sprintf(
-    "mongodb://%s:%s@%s:%s/%s?authSource=%s",
-    username, password, host, port, db_name, auth_db
-  )
+  db_url <- Sys.getenv("MONGO_ATLAS_URL")
   
   conn <- mongo(
     collection = collection_name,
